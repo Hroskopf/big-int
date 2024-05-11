2a364dc7e92a9dedeb1b42570a7b4ca45e1cf7c1
Oleksandr Hroskopf <oleksandrhroskopf@MacBook-Air-Oleksandr-2.local>
2024-05-11 21:34:12 +0200

    added multiplication (algorithm Karatsuba) and some small changes

--- bigInt.h
+++ bigInt.h
@@ -16,6 +16,20 @@ class bigInt{
         number = num;
         sign = sgn;
     }
+
+    bigInt multiple_by_ten_power(bigInt a, int n) const
+    {
+        if(a == 0)
+        return a;
+        auto b = a;
+        reverse(b.number.begin(), b.number.end());
+        for(int i=0;i<n;i++)
+        {
+            b.number.push_back(0);
+        }
+        reverse(b.number.begin(), b.number.end());
+        return b;
+    }
     
     public:
 
@@ -31,6 +45,22 @@ class bigInt{
         {
             number.push_back(v % 10);
             v /= 10;
+        }
+    }
+
+    bigInt(string s)
+    {
+        if(s[0]=='-')
+            sign = 0;
+        else
+            sign = 1;
+        number = {};
+        for(int i=s.size()-1;i>=0;i--)
+        {
+            if(s[i] != '-')
+            {
+                number.push_back(s[i] - '0');
+            }
         }
     }
 
@@ -132,7 +162,7 @@ class bigInt{
     bigInt operator-(const bigInt& other) const {
         if(sign and other.sign)
         {
-            if(*this > other)
+            if(*this >= other)
             {
                 bool borrow = 0;
                 vector<unsigned short>result;
@@ -183,4 +213,63 @@ class bigInt{
         return -other - (-*this);
     }
 
+    bigInt operator*(const bigInt& other) const {
+        auto a = *this;
+        auto b = other;
+        if(max(a.number.size(), b.number.size()) == 1)
+        {
+            int x = a.number[0];
+            int y = b.number[0];
+            if(!a.sign)
+            x = -x;
+            if(!b.sign)
+            y = -y;
+            return bigInt(x*y);
+        }
+        vector<unsigned short>A, B, C, D;
+        int n = max(a.number.size(), b.number.size()) / 2;
+        for(int i=0;i<max(a.number.size(), b.number.size());i++)
+        {
+            if(i<n)
+            {
+                if(i < a.number.size())
+                B.push_back(a.number[i]);
+                else
+                B.push_back(0);
+                if(i < b.number.size())
+                D.push_back(b.number[i]);
+                else
+                D.push_back(0);
+            }
+            else
+            {
+                if(i < a.number.size())
+                A.push_back(a.number[i]);
+                else
+                A.push_back(0);
+                if(i < b.number.size())
+                C.push_back(b.number[i]);
+                else
+                C.push_back(0);
+            }
+        }
+        bigInt A1 = bigInt(A, a.sign);
+        bigInt B1 = bigInt(B, a.sign);
+        bigInt C1 = bigInt(C, b.sign);
+        bigInt D1 = bigInt(D, b.sign);
+        bigInt X = (A1 + B1) * (C1 + D1);
+        bigInt Y = A1 * C1;
+        bigInt Z = B1 * D1;
+        return multiple_by_ten_power(Y, 2*n) + multiple_by_ten_power(X - Y - Z, n) + Z;
+    }
+
 };
+
+bigInt pow(bigInt a, int n)
+{
+    if(n==0)
+    return 1;
+    if(n%2==0)
+    return pow(a*a, n/2);
+    return a*pow(a, n-1);
+}
